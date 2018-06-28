af59e3756d19558f2c5486a0c1fdcc5e8135c54c
Mohanad Elhag <moh.7afiz@gmail.com>
2018-06-29 01:28:33 +0300

    Setup routers & Models

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+const items = require('./Routes/api/items')
+
+
 const app = express();
 
 // BodyParser Middleware
@@ -14,8 +17,12 @@ const db = require('./config/keys').mongoURI;
 mongoose
   .connect(db)
   .then(() => console.log('MongoDB Conneted..'))
-  .catch(err => console.log(err))
+  .catch(err => console.log(err));
 
+// Use Routes
+app.use('/api/items', items)
+
+// Port Config
 const port = process.env.PORT || 5000;
 
 app.listen(port, ()=> console.log(`Server started on port ${port}`))
